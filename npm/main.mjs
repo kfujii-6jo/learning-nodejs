5a340038d3a9db49aa0975cd993afcadb48fd0cd
kfujii03 <k.fujii@sprix.jp>
2024-04-28 01:54:36 +0900

    npm install lodash

--- npm/main.mjs
+++ npm/main.mjs
@@ -1,9 +1,12 @@
-import _ from "./lib/lodash.js";
+// import _ from "lodash"; // パスの指定がない場合はnode_modules配下のフォルダ名を参照する
+
+import cloneDeep from "lodash/cloneDeep.js";
 
 const original = { prop: { nested: "value" } };
 
 // オブジェクトの複製
-const cloned = _.cloneDeep(original);
+// const cloned = _.cloneDeep(original);
+const cloned = cloneDeep(original);
 
 cloned.prop.nested = "new value";
 
