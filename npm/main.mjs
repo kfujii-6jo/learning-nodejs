cdcbafeecdf2ec1b7e1e9541c59693e2f93d20fc
kfujii03 <k.fujii@sprix.jp>
2024-04-28 01:40:35 +0900

    import library

---
+++ npm/main.mjs
@@ -0,0 +1,12 @@
+import _ from "./lib/lodash.js";
+
+const original = { prop: { nested: "value" } };
+
+// オブジェクトの複製
+const cloned = _.cloneDeep(original);
+
+cloned.prop.nested = "new value";
+
+console.log(original, cloned);
+// npm main.mjs
+// { prop: { nested: 'value' } } { prop: { nested: 'new value' } }
